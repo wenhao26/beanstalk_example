8c62be8d3c8a3d54a86e146cfc8b572d4dcec808
Wu Wenhao <1321471639@qq.com>
2022-05-11 15:12:36 +0800

    add

--- example/producer.php
+++ example/producer.php
@@ -8,6 +8,56 @@ $bean = new Bean('192.168.7.206');
 
 // 查看状态信息
 //var_dump($bean->ph->stats());
+/*
+ *    'current-jobs-urgent' => '0', // 优先级小于1024状态为ready的job数量
+   'current-jobs-ready' => '0', // 状态为ready的job数量
+   'current-jobs-reserved' => '0', // 状态为reserved的job数量
+   'current-jobs-delayed' => '0', // 状态为delayed的job数量
+   'current-jobs-buried' => '0', // 状态为buried的job数量
+   'cmd-put' => '0', // 总共执行put指令的次数
+   'cmd-peek' => '0', // 总共执行peek指令的次数
+   'cmd-peek-ready' => '0', // 总共执行peek-ready指令的次数
+   'cmd-peek-delayed' => '0', // 总共执行peek-delayed指令的次数
+   'cmd-peek-buried' => '0', // 总共执行peek-buried指令的次数
+   'cmd-reserve' => '0', // 总共执行reserve指令的次数
+   'cmd-reserve-with-timeout' => '0',
+   'cmd-delete' => '0',
+   'cmd-release' => '0',
+   'cmd-use' => '0', // 总共执行use指令的次数
+   'cmd-watch' => '0', // 总共执行watch指令的次数
+   'cmd-ignore' => '0',
+   'cmd-bury' => '0',
+   'cmd-kick' => '0',
+   'cmd-touch' => '0',
+   'cmd-stats' => '2',
+   'cmd-stats-job' => '0',
+   'cmd-stats-tube' => '0',
+   'cmd-list-tubes' => '0',
+   'cmd-list-tube-used' => '0',
+   'cmd-list-tubes-watched' => '0',
+   'cmd-pause-tube' => '0',
+   'job-timeouts' => '0', // 所有超时的job的总共数量
+   'total-jobs' => '0', // 创建的所有job数量
+   'max-job-size' => '65535', // job的数据部分最大长度
+   'current-tubes' => '1', // 当前存在的tube数量
+   'current-connections' => '1', // 当前打开的连接数
+   'current-producers' => '0', // 当前所有的打开的连接中至少执行一次put指令的连接数量
+   'current-workers' => '0', // 当前所有的打开的连接中至少执行一次reserve指令的连接数量
+   'current-waiting' => '0', // 当前所有的打开的连接中执行reserve指令但是未响应的连接数量
+   'total-connections' => '2', // 总共处理的连接数
+   'pid' => '3609', // 服务器进程的id
+   'version' => '1.10', // 服务器版本号
+   'rusage-utime' => '0.000000', // 进程总共占用的用户CPU时间
+   'rusage-stime' => '0.001478', // 进程总共占用的系统CPU时间
+   'uptime' => '12031', // 服务器进程运行的秒数
+   'binlog-oldest-index' => '2', // 开始储存jobs的binlog索引号
+   'binlog-current-index' => '2', // 当前储存jobs的binlog索引号
+   'binlog-records-migrated' => '0',
+   'binlog-records-written' => '0', // 累积写入的记录数
+   'binlog-max-size' => '10485760', // binlog的最大容量
+   'id' => '37604ac4305d3b16', // 一个随机字符串，在beanstalkd进程启动时产生
+   'hostname' => 'localhost.localdomain',
+ */
 
 // 显示目前存在的管道
 //var_dump($bean->ph->listTubes());
